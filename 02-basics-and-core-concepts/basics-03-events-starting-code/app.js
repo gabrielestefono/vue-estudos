86078462ae3da007fd0de96b7abc7db3fe5e05c4
gabriel.estefono <gabrielestefono@hotmail.com>
2026-04-17 13:06:31 -0300

    Eventos e métodos

--- 02-basics-and-core-concepts/basics-03-events-starting-code/app.js
+++ 02-basics-and-core-concepts/basics-03-events-starting-code/app.js
@@ -1,31 +1,30 @@
 // @ts-check
+
 /**
- * @import {DataOutput} from './types/app';
+ * @this {{ counter: number }}
  */
+function increase() {
+  this.counter++;
+}
+
+/**
+ * @this {{ counter: number }}
+ */
+function decrease() {
+  if (this.counter > 0) {
+    this.counter--;
+  }
+}
 
 const app = Vue.createApp({
-  /**
-   *
-   * @returns {DataOutput}
-   */
   data() {
     return {
       counter: 0,
     };
   },
   methods: {
-    /**
-     * @this {DataOutput}
-     */
-    increase() {
-      this.counter = this.counter + 1;
-    },
-    /**
-     * @this {DataOutput}
-     */
-    decrease() {
-      this.counter === 0 ? 0 : (this.counter = this.counter - 1);
-    },
+    increase,
+    decrease,
   },
 });
 
