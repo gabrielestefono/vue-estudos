2ac72271276cf75053c048df77f9ecc084d80c13
gabriel.estefono <gabrielestefono@hotmail.com>
2026-04-28 06:58:33 -0300

    Finalizando monster slayer game

--- 04-the-monster-slayer-game/app.js
+++ 04-the-monster-slayer-game/app.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 /**
- * @import { DataOutput, MethodsOutput } from "./types/app";
+ * @import { DataOutput, MethodsOutput, ComputedOutput, AppOutput } from "./types/app";
  */
 
 /**
@@ -22,28 +22,134 @@ Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+      logMessages: [],
     };
+  },
+  computed: {
+    /**
+     * @this {AppOutput}
+     */
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    /**
+     * @this {AppOutput}
+     */
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
+    },
+    /**
+     * @this {AppOutput}
+     */
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    /**
+     * @this {AppOutput}
+     * @param {number} value
+     */
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    /**
+     * @this {AppOutput}
+     * @param {number} value
+     */
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
+    },
   },
   /**
    * @type {MethodsOutput}
    */
   methods: {
     /**
-     * @this {DataOutput & MethodsOutput}
+     * @this {AppOutput}
      */
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth = this.monsterHealth - attackValue;
-      setTimeout(() => {
-        this.attackPlayer();
-      }, 500);
+      this.attackPlayer();
+      this.addLogMessage("player", "attack", attackValue);
     },
     /**
-     * @this {DataOutput & MethodsOutput}
+     * @this {AppOutput}
      */
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth = this.playerHealth - attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
+    },
+    /**
+     * @this {AppOutput}
+     */
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+      this.addLogMessage("player", "attack", attackValue);
+    },
+    /**
+     * @this {AppOutput}
+     */
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth = this.playerHealth + healValue;
+      }
+      this.attackPlayer();
+      this.addLogMessage("player", "heal", healValue);
+    },
+    /**
+     * @this {AppOutput}
+     */
+    startNewGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
+    /**
+     * @this {AppOutput}
+     */
+    surrender() {
+      this.winner = "monster";
+    },
+    /**
+     * @this {AppOutput}
+     * @param {"player" | "monster"} who
+     * @param {"attack" | "heal"} what
+     * @param {number} value
+     */
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 }).mount("#game");
--- 04-the-monster-slayer-game/types/app.ts
+++ 04-the-monster-slayer-game/types/app.ts
@@ -3,6 +3,13 @@ import { MethodOptions } from "vue";
 export interface DataOutput {
   playerHealth: number;
   monsterHealth: number;
+  currentRound: number;
+  winner: null | "player" | "monster" | "draw";
+  logMessages: {
+    actionBy: "player" | "monster";
+    actionType: "attack" | "heal";
+    actionValue: number;
+  }[];
 }
 
 type Strict<T> = T & {
@@ -22,6 +29,27 @@ type RemoveIndexSignature<T> = {
 type StrictMethods = {
   attackMonster: () => void;
   attackPlayer: () => void;
+  specialAttackMonster: () => void;
+  healPlayer: () => void;
+  startNewGame: () => void;
+  surrender: () => void;
+  addLogMessage: (
+    who: "player" | "monster",
+    what: "attack" | "heal",
+    value: number,
+  ) => void;
 };
 
 export type MethodsOutput = RemoveIndexSignature<MethodOptions> & StrictMethods;
+
+export type ComputedOutput = {
+  monsterBarStyles: {
+    width: string;
+  };
+  playerBarStyles: {
+    width: string;
+  };
+  mayUseSpecialAttack: boolean;
+};
+
+export type AppOutput = DataOutput & MethodsOutput & ComputedOutput;
